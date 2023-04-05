2e372ed1bef55156c4e45c883d8604aa713e8ea7
Ushindi Gedeon <gedeontshobohwa1@gmail.com>
2023-04-05 15:49:03 +0200

    Format code

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const express = require("express");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 const app = express();
 
-app.use('/api', createProxyMiddleware({
-  target: 'https://bio.torre.co',
-  changeOrigin: true,
-  pathRewrite: {
-    '^/api': '/api',
-  },
-  onProxyRes: function(proxyRes, req, res) {
-    proxyRes.headers['Access-Control-Allow-Origin'] = '*';
-  },
-}));
+app.use(
+  "/api",
+  createProxyMiddleware({
+    target: "https://bio.torre.co",
+    changeOrigin: true,
+    pathRewrite: {
+      "^/api": "/api",
+    },
+    onProxyRes: function (proxyRes, req, res) {
+      proxyRes.headers["Access-Control-Allow-Origin"] = "*";
+    },
+  })
+);
 
 app.listen(3021, () => {
-  console.log('Proxy server listening on port 3001');
+  console.log("Proxy server listening on port 3001");
 });
